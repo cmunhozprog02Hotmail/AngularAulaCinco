4cf83ef6647a7e7c81627fcfc561c47864bc377a
cmunhozprog02h <cmunhozprog02@hotmail.com>
2020-02-09 13:34:17 -0300

    First Commit

---
+++ src/app/myfirst.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'myfirst',
+    template:`
+        <h1>My Fist Component</h1>
+    `
+})
+
+export class MyFirstComponent{
+
+}
